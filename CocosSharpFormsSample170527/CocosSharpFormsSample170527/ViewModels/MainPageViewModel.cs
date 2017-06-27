74260e1fd31dcc517847e78b76b39b116ce3482e
Tomohiro Suzuki <suzuchan@muf.biglobe.ne.jp>
2017-06-27 11:30:02 +0900

    リファクタ

--- CocosSharpFormsSample170527/CocosSharpFormsSample170527/ViewModels/MainPageViewModel.cs
+++ CocosSharpFormsSample170527/CocosSharpFormsSample170527/ViewModels/MainPageViewModel.cs
@@ -9,54 +9,19 @@ namespace CocosSharpFormsSample.ViewModels
 
 		public GameScene GameScene { get; set; }
 
-		public DelegateCommand TokyoCommand => new DelegateCommand(PlayAnimationTokyo);
-		public DelegateCommand KanagawaCommand => new DelegateCommand(PlayAnimationKanagawa);
-		public DelegateCommand SaitamaCommand => new DelegateCommand(PlayAnimationSaitama);
-		public DelegateCommand ChibaCommand => new DelegateCommand(PlayAnimationChiba);
-		public DelegateCommand GunmaCommand => new DelegateCommand(PlayAnimationGunma);
-		public DelegateCommand TochigiCommand => new DelegateCommand(PlayAnimationTochigi);
-		public DelegateCommand IbarakiCommand => new DelegateCommand(PlayAnimationIbaraki);
+		public DelegateCommand TokyoCommand => new DelegateCommand(() => GameScene.PlayAnimationTokyo());
+		public DelegateCommand KanagawaCommand => new DelegateCommand(() => GameScene.PlayAnimationKanagawa());
+		public DelegateCommand SaitamaCommand => new DelegateCommand(() => GameScene.PlayAnimationSaitama());
+		public DelegateCommand ChibaCommand => new DelegateCommand(() => GameScene.PlayAnimationChiba());
+		public DelegateCommand GunmaCommand => new DelegateCommand(() => GameScene.PlayAnimationGunma());
+		public DelegateCommand TochigiCommand => new DelegateCommand(() => GameScene.PlayAnimationTochigi());
+		public DelegateCommand IbarakiCommand => new DelegateCommand(() => GameScene.PlayAnimationIbaraki());
 
 		private INavigationService navigationService;
 
 		public MainPageViewModel(INavigationService navigationService)
 		{
 			this.navigationService = navigationService;
-		}
-
-		private void PlayAnimationTokyo()
-		{
-			GameScene.PlayAnimationTokyo();
-		}
-
-		private void PlayAnimationKanagawa()
-		{
-			GameScene.PlayAnimationKanagawa();
-		}
-
-		private void PlayAnimationSaitama()
-		{
-			GameScene.PlayAnimationSaitama();
-		}
-
-		private void PlayAnimationChiba()
-		{
-			GameScene.PlayAnimationChiba();
-		}
-
-		private void PlayAnimationGunma()
-		{
-			GameScene.PlayAnimationGunma();
-		}
-
-		private void PlayAnimationTochigi()
-		{
-			GameScene.PlayAnimationTochigi();
-		}
-
-		private void PlayAnimationIbaraki()
-		{
-			GameScene.PlayAnimationIbaraki();
 		}
 
 	}
